6412142b33c7efc1e1e675859ec4b2f79fc69712
David Corbin <daconex@gmail.com>
2018-02-21 13:04:10 -0500

    Added new icon and modified build script

--- GroupMe-Desktop.sh
+++ GroupMe-Desktop.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-npm install -g nativefier appdmg
+set -x
+
+rm -rf node_modules/ GroupMe-macOS.dmg
+npm install
+npm install nativefier -g
 
 nativefier https://web.groupme.com/chats \
   --name GroupMe \
   --single-instance \
   --platform linux \
+  --icon "icon.png" \
   --inject ./analytics.js \
   --inject ./groupme-custom.css
 nativefier https://web.groupme.com/chats \
   --name GroupMe \
   --single-instance \
   --platform osx \
+  --icon "icon.png" \
   --inject ./analytics.js \
   --inject ./groupme-custom.css
 nativefier https://web.groupme.com/chats \
   --name GroupMe \
   --single-instance \
   --platform win32 \
+  --icon "icon.png" \
   --inject ./analytics.js \
   --inject ./groupme-custom.css
 
-appdmg spec.json GroupMe-macOS.dmg
+./node_modules/appdmg/bin/appdmg.js spec.json GroupMe-macOS.dmg
 
